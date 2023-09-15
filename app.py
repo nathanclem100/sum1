f970bb8d5b288f65b8326e6358340f3f35f4d662
nathanclem100 <nathanclem100@gmail.com>
2023-09-15 17:40:44 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+print("Hello world")
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+sum = num1 + num2
+print("the sum of",num1,"and",num2,"is",sum)
+
+difference = num1 - num2
+print("the difference of",num1,"and",num2,"is",difference)
+
+product = num1 * num2
+print("the product of",num1,"and",num2,"is",product)
+
+quotient = num1 / num2
+print("the quotient of",num1,"and",num2,"is",quotient)
